4d017184ed82722b9a924c49e0856579837f7435
ValeryBMSTU <30847068+ValeryBMSTU@users.noreply.github.com>
2019-08-15 22:00:17 +0300

    Folder check

--- main.cpp
+++ main.cpp
@@ -28,18 +28,22 @@ freely, subject to the following restrictions:
 #include "stdlib.h"
 #include <fstream>
 #include <cstdint>
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
+#include <io.h>
+#include <dirent.h>
+#include <errno.h>
+// #include <experimental/filesystem> // C++-standard header file name
+// #include <filesystem> // Microsoft-specific implementation header file name
+// using namespace std::experimental::filesystem::v1;
 /*
 3 ways to decode a PNG from a file to RGBA pixel data (and 2 in-memory ways).
 */
 
 //g++ lodepng.cpp example_decode.cpp -ansi -pedantic -Wall -Wextra -O3
 
-
 //Example 1
 //Decode from disk to raw pixels with a single function call
-void decodeOneStep(const char* filename) {
+void decodeOneStep(const char *filename)
+{
     std::vector<unsigned char> image; //the raw pixels
     unsigned width, height;
 
@@ -47,13 +51,14 @@ void decodeOneStep(const char* filename) {
     unsigned error = lodepng::decode(image, width, height, filename);
 
     //if there's an error, display it
-    if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    if (error)
+        std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
     //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
 
     int curent_width = 0;
 
-    for (std::vector<unsigned char>::iterator it = image.begin() ; it!=image.end() ; it = it + 4)
+    for (std::vector<unsigned char>::iterator it = image.begin(); it != image.end(); it = it + 4)
     {
         if (curent_width == width)
         {
@@ -64,67 +69,86 @@ void decodeOneStep(const char* filename) {
         {
             curent_width++;
         }
-        
-        std::cout<<*it;
+
+        std::cout << *it;
     }
 }
 
 //Example 2
 //Load PNG file from disk to memory first, then decode to raw pixels in memory.
-void decodeTwoSteps(const char* filename) {
-  std::vector<unsigned char> png;
-  std::vector<unsigned char> image; //the raw pixels
-  unsigned width, height;
+void decodeTwoSteps(const char *filename)
+{
+    std::vector<unsigned char> png;
+    std::vector<unsigned char> image; //the raw pixels
+    unsigned width, height;
 
-  //load and decode
-  unsigned error = lodepng::load_file(png, filename);
-  if(!error) error = lodepng::decode(image, width, height, png);
+    //load and decode
+    unsigned error = lodepng::load_file(png, filename);
+    if (!error)
+        error = lodepng::decode(image, width, height, png);
 
-  //if there's an error, display it
-  if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    //if there's an error, display it
+    if (error)
+        std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
-  //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+    //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
 }
 
 //Example 3
 //Load PNG file from disk using a State, normally needed for more advanced usage.
-void decodeWithState(const char* filename) {
-  std::vector<unsigned char> png;
-  std::vector<unsigned char> image; //the raw pixels
-  unsigned width, height;
-  lodepng::State state; //optionally customize this one
+void decodeWithState(const char *filename)
+{
+    std::vector<unsigned char> png;
+    std::vector<unsigned char> image; //the raw pixels
+    unsigned width, height;
+    lodepng::State state; //optionally customize this one
 
-  unsigned error = lodepng::load_file(png, filename); //load the image file with given filename
-  if(!error) error = lodepng::decode(image, width, height, state, png);
+    unsigned error = lodepng::load_file(png, filename); //load the image file with given filename
+    if (!error)
+        error = lodepng::decode(image, width, height, state, png);
 
-  //if there's an error, display it
-  if(error) std::cout << "decoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+    //if there's an error, display it
+    if (error)
+        std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
-  //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
-  //State state contains extra information about the PNG such as text chunks, ...
+    //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+    //State state contains extra information about the PNG such as text chunks, ...
 }
 
-int main(int argc, char *argv[]) {
-
-    
+int main(int argc, char *argv[])
+{
     FILE *folder_check = NULL;
 
-    //folder_check = fopen("test.png", "r");
-    if (fs::exists("PNGfolder"))
-        std::cout<<std::endl<<"Папка не существует";
+    DIR *dir = opendir("PNGfolder11");
+    if (dir)
+    {
+        /* Directory exists. */
+        closedir(dir);
+    }
+    else if (ENOENT == errno)
+    {
+        /* Directory does not exist. */
+    }
     else
-        std::cout<<std::endl<<"Папка существует";
-    if (fs::exists("Hfolder"))
-        std::cout<<std::endl<<"Папка не существует";
-    else
-        std::cout<<std::endl<<"Папка существует";
-    if (fs::exists("PGMfolder"))
-        std::cout<<std::endl<<"Папка не существует";
-    else
-        std::cout<<std::endl<<"Папка существует";
+    {
+        /* opendir() failed for some other reason. */
+    }
 
-    const char* filename = argc > 1 ? argv[1] : "test.png";
+    //folder_check = fopen("test.png", "r");
+    // if (fs::exists("PNGfolder"))
+    //     std::cout<<std::endl<<"Папка не существует";
+    // else
+    //     std::cout<<std::endl<<"Папка существует";
+    // if (fs::exists("Hfolder"))
+    //     std::cout<<std::endl<<"Папка не существует";
+    // else
+    //     std::cout<<std::endl<<"Папка существует";
+    // if (fs::exists("PGMfolder"))
+    //     std::cout<<std::endl<<"Папка не существует";
+    // else
+    //     std::cout<<std::endl<<"Папка существует";
+
+    const char *filename = argc > 1 ? argv[1] : "test.png";
     decodeOneStep(filename);
     system("pause");
 }
-
